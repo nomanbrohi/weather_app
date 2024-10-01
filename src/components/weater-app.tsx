3f3117639c6e61d4fa0a22e7c6f3098eb7c10908
nomanbrohi <nomanbrohi9@gmail.com>
2024-10-01 22:01:22 +0500

    some responsive issue resolved

--- src/components/weater-app.tsx
+++ src/components/weater-app.tsx
@@ -30,15 +30,15 @@ export default function WeatherApp() {
 
   const weatherImage = () => {
     if (weather?.weather[0].main === "Rain") {
-      return <Image src="/images/rain.png" alt="rainy" />;
+      return <Image src="/images/rain.png" width={180} height={180} alt="rainy" />;
     } else if (weather?.weather[0].main === "Clear") {
-      return <Image src="/images/clear.png" alt="Clear" />;
+      return <Image src="/images/clear.png" width={180} height={180} alt="Clear" />;
     } else if (weather?.weather[0].main === "Clouds") {
-      return <Image src="/images/cloud.png" alt="cloudy" />;
+      return <Image src="/images/cloud.png" width={180} height={180} alt="cloudy" />;
     } else if (weather?.weather[0].main === "Mist") {
-      return <Image src="/images/mist.png" alt="mist" />;
+      return <Image src="/images/mist.png" width={180} height={180} alt="mist" />;
     } else if (weather?.weather[0].main === "Snow") {
-      return <Image src="/images/snow.png" alt="snow" />;
+      return <Image src="/images/snow.png" width={180} height={180} alt="snow" />;
     }
 
     return null;
@@ -77,13 +77,14 @@ export default function WeatherApp() {
           </h1>
         {/* parent Div */}
         <div
-          className={`bg-white/[.3] backdrop-blur-xl relative w-[400px] p-5 rounded-2xl border-2 border-solid border-inherit transition-all duration-500 ${
+          className={`bg-white/[.3] backdrop-blur-xl relative w-[300px] sm:w-[400px] p-5 rounded-2xl border-2 border-solid border-inherit transition-all duration-500 ${
             weather ? "h-[555px]" : "h-[100px]"
           }`}
         >
           <div className="relative w-full h-14 flex items-center bg-transparent">
             <MapPinIcon className="absolute left-2 w-5 h-5 text-white" />
             <input
+              className=" absolute w-[100%] h-full bg-transparent border-2 border-solid border-[rgba(255,255,255,.1)] outline-none rounded-xl text-sm text-white font-bold uppercase pr-12 pl-11 placeholder:text-white "
               placeholder="Enter Location"
               type="text"
               onChange={handleInput}
@@ -92,7 +93,6 @@ export default function WeatherApp() {
                   fetchWeather();
                 }
               }}
-              className=" absolute w-full h-full bg-transparent border-2 border-solid border-[rgba(255,255,255,.1)] outline-none rounded-xl text-2xl text-white font-bold uppercase pr-12 pl-11 placeholder:text-white "
             ></input>
             <button
               type="button"
@@ -105,8 +105,8 @@ export default function WeatherApp() {
           {/* Show Weather Info */}
           {weather && (
             <div className="mt-10 flex flex-col items-center">
-              <div className="w-[60%]">{weatherImage()}</div>
-              <p className="relative text-[64px] text-white leading-[1] font-bold mt-5 mb-2 ml-[-30]">
+              <div className="w-[100px] h-[100px] sm:w-[180px] sm:h-[180px]">{weatherImage()}</div>
+              <p className="relative sm:text-[64px] text-[34px] text-white leading-[1] font-bold sm:mt-5 mt-2 mb-2 ml-[-30]">
                 {weather.main.temp}
                 <span className="absolute text-[24px] ml-1">°C</span>
               </p>
@@ -114,18 +114,18 @@ export default function WeatherApp() {
                 {weather.weather[0].main}
               </p>
               <div className="flex justify-between items-center w-full text-2xl font-medium text-white capitalize mt-10">
-                <div className="flex justify-center gap-2">
+                <div className="sm:flex flex:col justify-center gap-2">
                   <i className="bx bx-water bx-rotate-45 text-4xl"></i>
                   <p>
                     {weather.main.humidity}
                     <span>%</span>
                   </p>
                 </div>
-                <div className="flex justify-center gap-2">
+                <div className="sm:flex flex:col text-center sm:justify-center gap-2">
                   <i className="bx bx-wind bx-rotate-45 text-4xl"></i>
                   <p>
                     {weather.wind.speed}
-                    <span>km/h</span>
+                    <span className="sm:text-2xl text-[14px] ">km/h</span>
                   </p>
                 </div>
               </div>
